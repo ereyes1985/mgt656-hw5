e63aad71bb710edb73fa611bf86d60a7dfdea06d
ereyes1985 <ernesto.reyes@yale.edu>
2015-10-09 20:26:48 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/nickname', function(request, response){
+    response.send('fair-tiger');
+});
+
+app.get('/anamaria', function(request, response){
+    response.send('Hola Ana Maria es Quique tu papi');
+});
+
 app.listen(process.env.PORT || 4000);
